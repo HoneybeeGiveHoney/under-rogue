683c9e7e76d172c9970d97c1f325361538d5ace4
HoneybeeGiveHoney <lolotrekpro111@gmail.com>
2026-09-02 00:27:55 +0300

    Integrity QTE almost done!

--- scenes/integrity.gd
+++ scenes/integrity.gd
@@ -12,22 +12,22 @@ var Beat = 1
 
 func _process(delta):
 #region Inputs
-	if Input.is_action_just_pressed("Right") and IsPlaying == 1:
+	if Input.is_action_just_pressed("Right") and IsPlaying == 1 and Failed == 0:
 		Right = 1
 		$Sounds/RightArrow.play()
 	if Input.is_action_just_released("Right") and IsPlaying == 1:
 		Right = 0
-	if Input.is_action_just_pressed("Left") and IsPlaying == 1:
+	if Input.is_action_just_pressed("Left") and IsPlaying == 1  and Failed == 0:
 		Left = 1
 		$Sounds/LeftArrow.play()
-	if Input.is_action_just_released("Left") and IsPlaying == 1:
+	if Input.is_action_just_released("Left") and IsPlaying == 1 :
 		Left = 0
-	if Input.is_action_just_pressed("Down") and IsPlaying == 1:
+	if Input.is_action_just_pressed("Down") and IsPlaying == 1 and Failed == 0:
 		Down = 1
 		$Sounds/DownArrow.play()
 	if Input.is_action_just_released("Down") and IsPlaying == 1:
 		Down = 0
-	if Input.is_action_just_pressed("Up") and IsPlaying == 1:
+	if Input.is_action_just_pressed("Up") and IsPlaying == 1 and Failed == 0:
 		Up = 1
 		$Sounds/UpArrow.play()
 	if Input.is_action_just_released("Up") and IsPlaying == 1:
@@ -46,11 +46,9 @@ func _process(delta):
 		$Sounds/Failure.play()
 	
 	
-	if Input.is_action_just_pressed("space") and Current == 1 and IsPlaying == 0:
+	if Input.is_action_just_pressed("space") and IsPlaying == 0:
 		SongStarted()
-	
-	if IsPlaying == 1:
-		$"../StatPointer".visible = true
+	if IsPlaying == 1 and Current == 1:
 		if Progress == 0:
 			$PointerMovement.play("Right")
 		if Progress == 1:
@@ -59,49 +57,46 @@ func _process(delta):
 			$PointerMovement.play("Up")
 		if Progress == 3:
 			$PointerMovement.play("Down")
-		
+	#region First Song
 		if Right == 1 and Progress == 0 and Failed == 0 and Beat == 1:
 			$DeadZone.start()
-			Beat = 0
-			$Beat.start()
+			DoBeat()
 		if Left == 1 and Progress == 0:
-			Failed = 1
+			Fail()
 		if Up == 1 and Progress == 0:
-			Failed = 1
+			Fail()
 		if Down == 1 and Progress == 0:
-			Failed = 1
+			Fail()
 	# second beat
 		if Right == 1 and Failed == 0 and Progress == 1 and Beat == 1:
 			$DeadZone.start()
-			Beat = 0
-			$Beat.start()
+			DoBeat()
 		if Left == 1 and Progress == 1:
-			Failed = 1
+			Fail()
 		if Up == 1 and Progress == 1:
-			Failed = 1
+			Fail()
 		if Down == 1 and Progress == 1:
-			Failed = 1
+			Fail()
 	# third beat
 		if Right == 1 and Progress == 2:
-			Failed = 1
+			Fail()
 		if Left == 1 and Progress == 2:
-			Failed = 1
+			Fail()
 		if Up == 1 and Failed == 0 and Progress == 2 and Beat == 1:
 			$DeadZone.start()
-			Beat = 0
-			$Beat.start()
+			DoBeat()
 		if Down == 1 and Progress == 2:
-			Failed = 1
+			Fail()
 	# fourth beat
 		if Right == 1 and Progress == 3:
-			Failed = 1
+			Fail()
 		if Left == 1 and Progress == 3:
-			Failed = 1
+			Fail()
 		if Up == 1 and Progress == 3:
-			Failed = 1
+			Fail()
 		if Down == 1  and Failed == 0 and Progress == 3 and Beat == 1:
-			Beat = 0
-			$Beat.start()
+			DoBeat()
+			Sucess()
 			GlobalData.IntegritySucess = 1
 		
 #endregion
@@ -111,12 +106,34 @@ func SongStarted():
 	IsPlaying = 1
 	GlobalData.CanFocus = 0
 	$Fail.start()
+	$"../Pointer/PointerAnimation".play("Appear")
+
+func Fail():
+	Failed = 1
+	$Sounds/Failure.play()
+	$"../Pointer/PointerAnimation".play("Dissapear")
+
+func Sucess():
+	IsPlaying = 0
+	GlobalData.CanFocus = 1
+	Right = 0
+	Left = 0
+	Up = 0
+	Down = 0
+	$Fail.stop()
+	$Sounds/Sucess.play()
+	$"../Pointer/PointerAnimation".play("Dissapear")
+
+func DoBeat():
+	$Beat.start()
+	Beat = 0
+	$"../Pointer/PointerAnimation".play("Beat")
 
 func _on_fail_timeout():
 	IsPlaying = 0
 	GlobalData.CanFocus = 1
 	$Sounds/Failure.play()
-	$"../StatPointer".visible = false
+	$"../Pointer".visible = false
 
 func _on_beat_timeout():
 	Beat = 1
