fa4c8444b3fc39bf09fe9223c9b86a222afcc624
HoneybeeGiveHoney <lolotrekpro111@gmail.com>
2026-09-21 23:47:46 +0300

    starting doing attacks

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -5,6 +5,6 @@ var BlockMovements = 0
 var CurrentWeapon = 0
 var Pulse = 0
 var CanFocus = 1
-
+var AttackCharges = 0
 
 var IntegritySucess = 0
--- Scripts/mobs/suferer.gd
+++ Scripts/mobs/suferer.gd
@@ -3,11 +3,12 @@ extends CharacterBody2D
 var speed = 200
 
 @export var knife_scene: PackedScene
+@export var Bell_scene: PackedScene
 @export var cooldown: = 0.1
-@export var knife_spawn_point: Node2D
+@export var spawn_point: Node2D
 var can_hit: bool = true
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta):
 	var direction = Vector2.ZERO
 	if Input.is_action_pressed("move_right") and GlobalData.IsPaused == 0:
 		direction.x += 1
@@ -34,38 +35,47 @@ func _physics_process(delta: float) -> void:
 	velocity = direction * speed
 	move_and_slide()
 
-func _process(_delta: float):
+	$Attacks/Cooldown.wait_time = cooldown
+	$InteractBox.position = get_local_mouse_position()
+	
+func _process(_delta):
 	if Input.is_action_pressed("attack") and can_hit and GlobalData.CurrentWeapon == 1:
 		$sounds/KnifeSwing.play()
 		if GlobalData.Pulse < 1.2:
 			GlobalData.Pulse += 0.1
 		attack_knife()
+	
+	if Input.is_action_pressed("attack") and can_hit and GlobalData.CurrentWeapon == 2:
+		attack_integrity()
 
 func attack_knife():
 	can_hit = false
+	$Attacks/Cooldown.start()
 	var knife = knife_scene.instantiate()
 	get_tree().current_scene.add_child(knife)
 	var direction = (get_global_mouse_position() - global_position).normalized()
 	knife.global_position = global_position + direction * 10
 	knife.rotation = direction.angle()
 	knife.setup(direction)
-	var timer = get_tree().create_timer(cooldown)
-	timer.timeout.connect(func(): can_hit = true)
-
-func attack_spin():
-	pass
+	
+func attack_integrity():
+	can_hit = false
+	$Attacks/Cooldown.start()
+	var Bell = Bell_scene.instantiate()
+	get_tree().current_scene.add_child(Bell)
+	Bell.position = get_local_mouse_position()
 	
 @export var rmb_attack_scene: PackedScene
 @export var rmb_cooldown: float = 0.3
 var can_rmb_attack: bool = true
 
-func _input(event: InputEvent) -> void:
+func _input(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
 			if can_rmb_attack and GlobalData.IsPaused == 0:
 				attack_rmb()
 
-func attack_rmb() -> void:
+func attack_rmb():
 	can_rmb_attack = false
 	var direction = (get_global_mouse_position() - global_position).normalized()
 
@@ -79,3 +89,5 @@ func attack_rmb() -> void:
 	var timer = get_tree().create_timer(rmb_cooldown)
 	timer.timeout.connect(func(): can_rmb_attack = true)
 	
+func _on_cooldown_timeout():
+	can_hit = true
